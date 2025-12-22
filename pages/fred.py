93e05eab747c42b5d4462f6d1431d5c713c0a1e9
laldee21-oss <laldee21@gmail.com>
2025-12-21 19:28:01 -0500

    Update fred.py

--- pages/fred.py
+++ pages/fred.py
@@ -163,11 +163,6 @@ def show():
     </script>
     """, unsafe_allow_html=True)
 
-    # Back button
-    if st.button("← Back to Home (Fred)", key="fred_back_button"):
-        st.session_state.current_page = "home"
-        st.rerun()
-
     # Hero image
     st.image("https://i.postimg.cc/fRms9xv6/tierra-mallorca-rg-J1J8SDEAY-unsplash.jpg", caption="Your Keys Await – Welcome to your longevity lifestyle")
 
